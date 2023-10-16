c4372aee0dd6e8724ebe4b9b466de6ff3338fa97
DarkSquirr3l <108667457+DarkSquirr3l@users.noreply.github.com>
2023-10-16 12:50:50 +0100

    completed salary calculator

--- src/chapter3/SalaryCalculator.java
+++ src/chapter3/SalaryCalculator.java
@@ -1,15 +1,32 @@
 package chapter3;
 
+import java.util.Scanner;
+
+/*
+All salespeople get a salary of £1,000 a week.
+A £250 bonus is paid for every 10 sales.
+ */
+
 public class SalaryCalculator {
 
     public static void main(String[] args){
 
         //Initialize know values
+        int salary = 1_000;
+        int bonus = 250;
 
         //Get values for the unknown
+        System.out.println("How many sales did the employee make this week?");
+        Scanner sc = new Scanner(System.in);
+        int sales = sc.nextInt();
+        sc.close();
 
         //Bonus calculation
-
+        if (sales > 9) {
+            int totalBonusPayments = sales / 10;
+            salary += bonus * totalBonusPayments;
+        }
         //Output
+        System.out.println("The employee's pay is £" + salary);
     }
 }
