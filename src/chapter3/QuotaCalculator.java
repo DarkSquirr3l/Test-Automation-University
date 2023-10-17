a19b8a849aacc12c825c093d3d4d3a160f9b1627
DarkSquirr3l <108667457+DarkSquirr3l@users.noreply.github.com>
2023-10-17 12:25:10 +0100

    Quota calculator added - tested and working

---
+++ src/chapter3/QuotaCalculator.java
@@ -0,0 +1,25 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class QuotaCalculator {
+    public static void main(String[] args) {
+
+        // Initialize values we know.
+        int quota = 10;
+
+        // Get unknown values
+        System.out.println("Enter the number of sales you made this week:");
+        Scanner sc = new Scanner(System.in);
+        int sales = sc.nextInt();
+        sc.close();
+
+        // Make a decision on the path to take
+        if (sales >= quota){
+            System.out.println("Congrats! You met your quota.");
+        } else {
+            int salesShort = quota - sales;
+            System.out.println("You were " + salesShort + " sales short. :(");
+        }
+    }
+}
