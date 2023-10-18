2007ab5e2a3f7052cd54eaf18cc0b8c567fb5953
DarkSquirr3l <108667457+DarkSquirr3l@users.noreply.github.com>
2023-10-18 16:09:34 +0100

    Create LetterSearch.java

---
+++ src/chapter4/LetterSearch.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class LetterSearch {
+
+    public static void main(String[] args) {
+
+        //Get test to search through
+        System.out.println("Enter a line of text:");
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
