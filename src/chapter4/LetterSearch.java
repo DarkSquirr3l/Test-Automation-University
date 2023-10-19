373f8650577536de3e62ddfbc6c7fb167889e74e
DarkSquirr3l <108667457+DarkSquirr3l@users.noreply.github.com>
2023-10-19 11:40:38 +0100

    LetterSeacher class added - tested and working.

--- src/chapter4/LetterSearch.java
+++ src/chapter4/LetterSearch.java
@@ -9,6 +9,25 @@ public class LetterSearch {
         //Get test to search through
         System.out.println("Enter a line of text:");
         Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+        sc.close();
+
+        //Search the text for the letter A
+        boolean letterFound = false;
+
+        for (int i=0; i < text.length(); i++) {
+            char currentLetter = text.charAt(i);
+            if (currentLetter == 'A' || currentLetter == 'a') {
+                letterFound = true;
+                break;
+            }
+        }
+
+        if (letterFound) {
+            System.out.println("This text contains the letter 'A'.");
+        } else {
+            System.out.println("This text does not contain the letter 'A'.");
+        }
         
     }
 }
