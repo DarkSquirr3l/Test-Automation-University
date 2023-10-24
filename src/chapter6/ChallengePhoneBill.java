7be97618998a6b70ceb1a48806449ad8b18a0a6d
DarkSquirr3l <108667457+DarkSquirr3l@users.noreply.github.com>
2023-10-24 15:37:14 +0100

    ChallengePhoneBill complete - removed magic numbers, tested & working.

--- src/chapter6/ChallengePhoneBill.java
+++ src/chapter6/ChallengePhoneBill.java
@@ -2,6 +2,9 @@ package chapter6;
 
 public class ChallengePhoneBill {
 
+    private static final double OVERAGE_RATE = 0.1;
+    private static final double TAX_RATE = 0.2;
+
     private static int count = 0;
     private double baseCost;
     private double planMinutes;
@@ -12,22 +15,23 @@ public class ChallengePhoneBill {
         setBaseCost(20);
         setPlanMinutes(600);
         setId("BILL" + count);
-        setOverage(0);
+        setMinutesUsed(0);
         count++;
     }
 
     public ChallengePhoneBill(int id){
         setBaseCost(20);
         setPlanMinutes(600);
-        setId("BILL" + id);
-        setOverage(0);
+        setId("CSTM" + id);
+        setMinutesUsed(0);
     }
 
     public ChallengePhoneBill(double baseCost, double planMinutes, double minutesUsed) {
         setBaseCost(baseCost);
         setPlanMinutes(planMinutes);
-        setId("BILL" + id);
-        setOverage(minutesUsed);
+        setId("BILL" + count);
+        setMinutesUsed(minutesUsed);
+        count++;
     }
 
     public void setBaseCost(double baseCost) {
@@ -48,11 +52,39 @@ public class ChallengePhoneBill {
 
     public String getId(){return id;}
 
-    public void setOverage(double minutesUsed){
+    public void setMinutesUsed(double minutesUsed){
         this.minutesUsed=minutesUsed;
     }
 
-    public double getOverage(){return minutesUsed;}
+    public double getMinutesUsed(){return minutesUsed;}
+
+    public double minutesOver(){
+        return Math.max(0, getMinutesUsed() - getPlanMinutes());
+    }
+
+    public double calculateOverage(){
+        return minutesOver() * OVERAGE_RATE;
+    }
+
+    public double calculatePreTaxTotal(){
+        return getBaseCost() + calculateOverage();
+    }
+
+    public double calculateTax(){
+        return calculatePreTaxTotal() * TAX_RATE;
+    }
+
+    public void generateBill(){
+        System.out.println("*** PHONE BILL ***");
+        System.out.println("Bill ID: " + getId());
+        System.out.println("Minutes used: " + String.format("%.2f", getMinutesUsed()));
+        System.out.println("Minutes over: " + String.format("%.2f", minutesOver()));
+        System.out.println("Base cost: £" + String.format("%.2f", getBaseCost()));
+        System.out.println("Extra cost: £" + String.format("%.2f", calculateOverage()));
+        System.out.println("Tax: £" + String.format("%.2f", calculateTax()));
+        System.out.println("Total: £" + String.format("%.2f", (calculatePreTaxTotal() + calculateTax())));
+        System.out.println(" ");
+    }
 }
 
 
--- src/chapter6/ChallengePhoneBillBuilder.java
+++ src/chapter6/ChallengePhoneBillBuilder.java
@@ -4,6 +4,14 @@ public class ChallengePhoneBillBuilder {
 
     public static void main(String[] args){
 
-        // bill creation handled by other class
+        ChallengePhoneBill bill1 = new ChallengePhoneBill();
+        ChallengePhoneBill bill2 = new ChallengePhoneBill(123);
+        ChallengePhoneBill bill3 = new ChallengePhoneBill(25, 500, 400);
+        ChallengePhoneBill bill4 = new ChallengePhoneBill(20, 420, 851);
+
+        bill1.generateBill();
+        bill2.generateBill();
+        bill3.generateBill();
+        bill4.generateBill();
     }
 }
