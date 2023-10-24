be29126d4ce0e72292a865e4999b0f93526c19db
DarkSquirr3l <108667457+DarkSquirr3l@users.noreply.github.com>
2023-10-24 13:08:37 +0100

    HomeAreaCalculatorRedo added - complete - learnt more about static

--- src/chapter6/HomeAreaCalculatorRedo.java
+++ src/chapter6/HomeAreaCalculatorRedo.java
@@ -4,24 +4,33 @@ import java.util.Scanner;
 
 public class HomeAreaCalculatorRedo {
 
+    private Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args){
 
         HomeAreaCalculatorRedo calculator = new HomeAreaCalculatorRedo();
         Rectangle kitchen = calculator.getRoom();
         Rectangle bathroom = calculator.getRoom();
+
+        calculator.sc.close();
+
+        double area = calculator.calculateTotalArea(kitchen, bathroom);
+
+        System.out.println("The total area of the rooms is: " + area);
     }
 
     public Rectangle getRoom(){
-
-        Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the length of your room:");
         double length = sc.nextDouble();
         System.out.println("Enter the width of your room:");
         double width = sc.nextDouble();
 
-        sc.close();
-
         return new Rectangle(length, width);
+    }
+
+    public double calculateTotalArea(Rectangle rectangle1, Rectangle rectangle2) {
+
+        return rectangle1.calculateArea() * rectangle2.calculateArea();
     }
 }
