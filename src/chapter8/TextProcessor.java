2e62cc6ebf74bb7846ea6f1e45671a8ddd6bd6c6
DarkSquirr3l <108667457+DarkSquirr3l@users.noreply.github.com>
2023-10-26 14:07:48 +0100

    TextProcessor class added

---
+++ src/chapter8/TextProcessor.java
@@ -0,0 +1,32 @@
+package chapter8;
+
+public class TextProcessor {
+
+    public static void main(String[] args){
+       //countWords("I'm looking forward to completing this course!");
+        reverseString("Scooby Doo");
+    }
+
+    /**
+     * splits a string into an array by tokenizing it
+     * @param text full string to be split
+     */
+    public static void countWords(String text){
+        var words = text.split(" ");
+        int numberOfWords = words.length;
+
+
+        String message = String.format("Your text contains %d words:", numberOfWords);
+        System.out.println(message);
+
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+
+    public static void reverseString(String text){
+        for(int i=text.length()-1; i>=0; i--){
+            System.out.print(text.charAt(i));
+        }
+    }
+}
