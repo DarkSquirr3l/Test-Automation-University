cd20b432f801a3151e28072205c5d96035ffedc4
DarkSquirr3l <108667457+DarkSquirr3l@users.noreply.github.com>
2023-11-02 13:36:18 +0000

    Chapter 13 - Exceptions handling class complete

---
+++ src/chapter13/ExceptionHandling.java
@@ -0,0 +1,44 @@
+package chapter13;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args) {
+
+        //createNewFile();
+        numbersExceptionHandling();
+    }
+
+    public static void createNewFile(){
+        File file = new File("resources/invisible.txt");
+        try{
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Directory doesn't exist.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void numbersExceptionHandling() {
+        File file = new File("numbers/invisible.txt");
+        try{
+            Scanner fileReader = new Scanner(file);
+
+            while(fileReader.hasNext()){
+                double num = fileReader.nextDouble();
+                System.out.println(num);
+            }
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+        } catch(InputMismatchException e){
+            e.printStackTrace();
+        } finally {
+            System.out.println("Finally"); // executes every time
+        }
+    }
+}
